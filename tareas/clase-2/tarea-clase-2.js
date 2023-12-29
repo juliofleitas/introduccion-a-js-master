fc01c44e967b12c167c0a47745c4c0525b52a9b7
jotadev <jfleitas.it@gmail.com>
2023-12-29 00:31:57 -0300

    tarea2

--- tareas/clase-2/tarea-clase-2.js
+++ tareas/clase-2/tarea-clase-2.js
@@ -18,6 +18,37 @@
 
 // TAREA: Cambiar el operador a '-', y fijarse que llame a la función 'restar' en vez de a la de 'sumar'.
 
+
+
+function sumar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function restar(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+const operador = prompt('Ingrese + para sumar o - para restar dos numeros');
+const numero1 = 1;
+const numero2 = 2;
+
+let resultado;
+if (operador === '+') {
+    resultado = sumar(numero1, numero2);
+} else {
+    resultado = restar(numero1, numero2);
+}
+
+
+console.log(
+    `El resultado de ${numero1} ${operador} ${numero2} es ${resultado}`
+);
+
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/template_strings
+
+
+/*
+
 function sumar(numero1, numero2) {
     return numero1 + numero2;
 }
@@ -37,8 +68,9 @@ if (operador === '+') {
     resultado = restar(numero1, numero2);
 }
 
-//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/template_strings
+
 console.log(
     `El resultado de ${numero1} ${operador} ${numero2} es ${resultado}`
 );
 
+*/
